024b2985fad811a21d1ca334e57cebb556c97166
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-06-16 15:06:02 +0200

    Setup dart cli project

---
+++ bin/password_manager.dart
@@ -0,0 +1,5 @@
+import '../lib/password_manager.dart' as password_manager;
+
+void main(List<String> arguments) {
+  print('Hello world: ${password_manager.calculate()}!');
+}
---
+++ test/password_manager_test.dart
@@ -0,0 +1,8 @@
+import '../lib/password_manager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
