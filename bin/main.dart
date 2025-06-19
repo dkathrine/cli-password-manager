ec2945bb27bd69ee15cac3580c4ec3717426842e
Kathrine <101878099+dkathrine@users.noreply.github.com>
2025-06-19 15:28:32 +0200

    implemented register function
    - implemented register function
    - restructured how files are saved
    - rewrote login to account for the now existing register
    - added color to the print();
    - changed name of project in .yaml file

--- bin/main.dart
+++ bin/main.dart
@@ -7,32 +7,52 @@ Future<void> main() async {
   // Initialize the encryption service and vault manager
   final encryptionService = EncryptionService();
   final vaultManager = VaultManager(encryptionService);
-  print('Welcome to the Password Manager!');
 
-  // Check if the vault file exists in the home directory
-  // If it exists, load the vault; otherwise, create a new vault
-  final homeDir =
-      Platform.environment['HOME'] ??
-      Platform.environment['USERPROFILE'] ??
-      '.';
-  final file = File('$homeDir/vault.json');
-  if (await file.exists()) {
-    print('🔍 Vault file found. Loading existing vault...');
-    await vaultManager.loadVault();
-  } else {
-    print('❗ Vault file not found. A new vault will be created.');
-  }
+  // bool variable called exit, default value is false
+  bool exit = false;
+  // as long as exit is false the program will run
+  while (!exit) {
+    print('\x1B[1;35mWelcome to the CLI Password Manager!\x1B[0m');
 
-  // Prompt the user for the master password
-  print('Please enter your master password:');
-  final masterPassword = stdin.readLineSync().toString() ?? '';
-  // If the master password is empty, exit the program
-  if (masterPassword.isEmpty) {
-    print('❌ Master password cannot be empty.');
-    return;
+    // Check if the vault file exists in the home directory
+    // If it exists, load the vault; otherwise, create a new vault
+
+    print('\x1B[32m1. ✍️  Register\n\x1B[34m2. 🚪 Login\x1B[0m');
+    final option = stdin.readLineSync();
+
+    print('Please enter your username:');
+    final username = stdin.readLineSync() ?? '';
+
+    if (username.isEmpty) {
+      print('❌ \x1B[31mUsername cannot be empty.\x1B[0m');
+      return;
+    }
+
+    // Prompt the user for the master password
+    print('Please enter your master password:');
+    final masterPassword = stdin.readLineSync() /* .toString() */ ?? '';
+    // If the master password is empty, exit the program
+    if (masterPassword.isEmpty) {
+      print('❌ \x1B[31mMaster password cannot be empty.\x1B[0m');
+      return;
+    }
+
+    bool success = false;
+
+    switch (option) {
+      case '1':
+        success = await vaultManager.register(username, masterPassword);
+        break;
+      case '2':
+        success = await vaultManager.login(username, masterPassword);
+        break;
+      default:
+        print('❌ \x1B[31mInvalid option selected.\x1B[0m');
+    }
+
+    if (success) {
+      // Display the main menu
+      exit = await vaultManager.menu();
+    }
   }
-  // Login to the vault manager with the master password
-  await vaultManager.login(masterPassword);
-  // Display the main menu
-  await vaultManager.menu();
 }
